7618c810afb4df0bf3c5d6268bcd037d6681b953
charlotte <charlottelee849@gmail.com>
2022-03-06 10:54:13 +0800

    加入modal

--- src/views/AdminProducts.vue
+++ src/views/AdminProducts.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="container">
+    <Loading :active="isLoading"></Loading>
     <h2>後台產品管理列表</h2>
     <div class="text-end mt-4">
       <button
@@ -13,6 +14,7 @@
     <table class="table mt-4">
       <thead>
         <tr>
+          <th width="120">圖片</th>
           <th width="120">分類</th>
           <th>產品名稱</th>
           <th width="120">原價</th>
@@ -23,6 +25,16 @@
       </thead>
       <tbody>
         <tr v-for="item in products" :key="item.id">
+          <td style="width: 200px">
+            <div
+              style="
+                height: 100px;
+                background-size: cover;
+                background-position: center;
+              "
+              :style="{ backgroundImage: `url(${item.imageUrl})` }"
+            ></div>
+          </td>
           <td>{{ item.category }}</td>
           <td>{{ item.title }}</td>
           <td class="text-end">
@@ -70,19 +82,40 @@
     <!-- 1.分頁元件 -->
     <Pagination :pages="pagination" @get-product="getProducts"></Pagination>
     <!-- 分頁元件 -->
+    <!-- AdminProductModal -->
+    <AdminProductModal
+      :temp-product="tempProduct"
+      :is-new="isNew"
+      ref="adminProductModal"
+      @get-products="getProducts"
+    ></AdminProductModal>
+    <!-- DelProductModal model -->
+    <DelProductModal
+      :temp-product="tempProduct"
+      ref="delProductModal"
+      @get-products="getProducts"
+    ></DelProductModal>
   </div>
 </template>
 
 <script>
 import Pagination from "@/components/Pagination.vue";
+import AdminProductModal from "@/components/AdminProductModal.vue";
+import DelProductModal from "@/components/DelProductModal.vue";
 
 export default {
   data() {
     return {
       products: [],
+      isLoadingItem: "", // 6. 局部讀取效果的變數
+      isLoading: false,
       isNew: false,
       tempProduct: {
         imagesUrl: [],
+      },
+      modal: {
+        AdminProductModal: "",
+        DelProductModal: "",
       },
       // 1.傳入分頁資訊
       pagination: {},
@@ -92,18 +125,23 @@ export default {
   // 1.區域註冊
   components: {
     Pagination,
+    AdminProductModal,
+    DelProductModal,
   },
   methods: {
     //取得產品列表
     getProducts(page = 1) {
+      this.isLoading = true;
       //陣列包物件
-      const api = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/admin/products/?page=${page}`;
+      const url = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/admin/products?page=${page}`;
       //物件包物件
       //const url = `${api}/api/${path}/admin/products/all`;
       this.$http
-        .get(api)
+        .get(url)
         .then((res) => {
+          console.log(res);
           this.products = res.data.products;
+          this.isLoading = false;
           this.pagination = res.data.pagination;
         })
         .catch((err) => {
@@ -118,6 +156,31 @@ export default {
       //   console.log(this.products[key], key);
       // })
     },
+    openModal(status, product) {
+      console.log(status, product);
+      if (status === "isNew") {
+        this.tempProduct = {
+          imagesUrl: [],
+        };
+        this.isNew = true;
+        this.$refs.adminProductModal.openModal();
+      } else if (status === "edit") {
+        this.tempProduct = JSON.parse(JSON.stringify(product));
+        this.isNew = false;
+        this.$refs.adminProductModal.openModal();
+        if (!this.tempProduct.imagesUrl) {
+          this.tempProduct.imagesUrl = [];
+        }
+      } else if (status === "delete") {
+        this.tempProduct = JSON.parse(JSON.stringify(product));
+        this.$refs.delProductModal.openModal();
+      }
+    },
+  },
+  //初使化
+  mounted() {
+    //把getProducts產品列表運作起來
+    this.getProducts();
   },
 };
 </script>
